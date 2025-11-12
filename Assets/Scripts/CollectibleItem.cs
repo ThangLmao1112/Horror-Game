649b21a20f33ab38d9b158c10557a1ff7fbf41cc
ThangLmao1112 <thang14563@gmail.com>
2025-11-12 17:15:09 +0700

    TamOn

---
+++ Assets/Scripts/CollectibleItem.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class CollectibleItem : MonoBehaviour
+{
+    [Header("Thu thập vật phẩm")]
+    public string itemName = "Manh mối bí ẩn";
+    public AudioClip collectSound; // Âm thanh khi thu thập (tùy chọn)
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Kiểm tra xem có phải người chơi chạm vào không
+        if (other.CompareTag("Player"))
+        {
+            CollectItem();
+        }
+    }
+
+    void CollectItem()
+    {
+        // Thông báo cho GameManager
+        if (GameManager.Instance != null)
+        {
+            GameManager.Instance.CollectClue();
+        }
+
+        // Phát âm thanh (nếu có)
+        if (collectSound != null)
+        {
+            AudioSource.PlayClipAtPoint(collectSound, transform.position);
+        }
+
+        // Xóa vật phẩm khỏi game
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance; // Singleton để truy cập từ mọi nơi
+
+    [Header("UI")]
+    public Text clueCounterText;
+
+    [Header("Gameplay")]
+    public int totalClues = 5; // Tổng số manh mối cần tìm
+    private int collectedClues = 0;
+
+    void Awake()
+    {
+        // Thiết lập Singleton
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        UpdateUI();
+    }
+
+    public void CollectClue()
+    {
+        collectedClues++;
+        UpdateUI();
+
+        // Kiểm tra thắng game
+        if (collectedClues >= totalClues)
+        {
+            WinGame();
+        }
+    }
+
+    void UpdateUI()
+    {
+        if (clueCounterText != null)
+        {
+            clueCounterText.text = "Manh mối: " + collectedClues + "/" + totalClues;
+        }
+    }
+
+    void WinGame()
+    {
+        Debug.Log("THẮNG! Đã tìm đủ tất cả manh mối!");
+        clueCounterText.text = "HOÀN THÀNH! Bạn đã tìm ra bí mật!";
+    }
+}
